b89bd097211170ddef63ce3d30d008668a64d26c
Jerome Etienne <jerome.etienne@gmail.com>
2012-05-04 19:03:11 +0200

    moved in its own project

--- examples/planets/cameracontrols.js
+++
@@ -1,9 +0,0 @@
-function initCameraControls(world)
-{
-	var controls	= new THREEx.DragPanControls(world.camera());
-
-	//var controls	= new THREE.FirstPersonControls(world.camera());
-	//var controls	= new THREE.TrackballControls(world.camera());
-
-	world.setCameraControls(controls);
-}
