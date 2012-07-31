79dd86de877431d7825b47226fbf89ce975228db
Jerome Etienne <jerome.etienne@gmail.com>
2012-07-31 15:49:35 +0200

    handle webaudio.mute/toggleMute - fix #10

--- src/webaudio.core.js
+++ src/webaudio.core.js
@@ -47,6 +47,10 @@ WebAudio	= function(){
 	// create the context
 	this._ctx	= new webkitAudioContext();
 
+	// setup internal variable
+	this._muted	= false;
+	this._volume	= 1;
+	
 	// setup the end of the node chain
 	// TODO later code the clipping detection from http://www.html5rocks.com/en/tutorials/webaudio/games/ 
 	this._gainNode	= this._ctx.createGainNode();
@@ -114,8 +118,28 @@ WebAudio.prototype._entryNode	= function(){
  * getter/setter on the volume
 */
 WebAudio.prototype.volume	= function(value){
-	if( value === undefined )	return this._gainNode.gain.value;
-	this._gainNode.gain.value	= value;
+	if( value === undefined )	return this._volume;
+	// update volume
+	this._volume	= value;
+	// update actual volume IIF not muted
+	if( this._muted  === false ){
+		this._gainNode.gain.value	= this._volume;	
+	}
+	// return this for chained API
 	return this;
 };
 
+/** 
+ * gett/setter for mute
+*/
+WebAudio.prototype.mute	= function(value){
+	if( value === undefined )	return this._muted;
+	this._muted	= value;
+	this._gainNode.gain.value	= this._muted ? 0 : this._volume;
+	return this;	// for chained API
+}
+
+WebAudio.prototype.toggleMute	= function(){
+	if( this.mute() )	this.mute(false);
+	else			this.mute(true);
+}
